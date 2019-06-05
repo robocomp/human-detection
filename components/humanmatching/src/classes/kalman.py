b1d3d601067002b254c3f711890b4925210506d7
aracelivegamagro <aracelivegamagro@hotmail.com>
2019-06-05 11:27:44 +0200

    Modified kalman with acceleration. Now the matrix Q is calculated correctly

--- components/humanmatching/src/classes/kalman.py
+++ components/humanmatching/src/classes/kalman.py
@@ -3,18 +3,16 @@ from filterpy.common import Q_discrete_white_noise
 from filterpy.kalman import KalmanFilter
 from filterpy.stats import plot_covariance_ellipse, plot_covariance
 from scipy.linalg import block_diag
-import faulthandler
-faulthandler.enable()
+# import faulthandler
+# faulthandler.enable()
 import matplotlib.pyplot as plt
-
 
 
 class KalmanTracker(KalmanFilter):
     def __init__(self, x=0, y=0):
         super(KalmanTracker, self).__init__(dim_x=4, dim_z=2)
         # Measurement Function
-        self.init_to_position(x,y)
-
+        self.init_to_position(x, y)
 
     def init_to_position(self, x, y):
         self.H = np.array([[1, 0, 0, 0],
@@ -32,8 +30,7 @@ class KalmanTracker(KalmanFilter):
         # Covariance Matrix
         self.P = np.eye(4) * 2.5
 
-
-    def predict_with_time_diff(self,dt):
+    def predict_with_time_diff(self, dt):
         self.F = np.array([[1, dt, 0, 0],
                            [0, 1, 0, 0],
                            [0, 0, 1, dt],
@@ -47,40 +44,36 @@ class KalmanTracker(KalmanFilter):
 
         return self.x, self.P
 
-    def update(self,z):
+    def update(self, z):
         super(KalmanTracker, self).update(z)
         return self.x, self.P
 
 
 def plotResults(measurements, predictions, covariances):
+    for pos_vel, cov_matrix in zip(predictions, covariances):
+        cov = np.array([[cov_matrix[0, 0], cov_matrix[2, 0]], [cov_matrix[0, 2], cov_matrix[2, 2]]])
+        # cov = np.array([[cov_matrix[1, 1], cov_matrix[3, 1]],[cov_matrix[1, 3], cov_matrix[3, 3]]])
 
-        for pos_vel, cov_matrix in zip(predictions, covariances):
+        [x_pos, x_vel, y_pos, y_vel] = pos_vel[:, 0]
+        mean = (x_pos, y_pos)
+        # Plotting elipses
+        plot_covariance_ellipse(mean, cov=cov, fc='r', alpha=0.20, show_semiaxis=True)
 
-            # cov = np.array([[cov_matrix[0, 0], cov_matrix[2, 0]],[cov_matrix[0, 2], cov_matrix[2, 2]]])
-            cov = np.array([[cov_matrix[1, 1], cov_matrix[3, 1]],[cov_matrix[1, 3], cov_matrix[3, 3]]])
+        # plotting velocity
+        plt.quiver(x_pos, y_pos, x_vel, y_vel, color='b', scale_units='xy', scale=1, alpha=0.50, width=0.005)
 
-            [x_pos, x_vel, y_pos, y_vel] = pos_vel[:,0]
-            mean = (x_pos, y_pos)
-            # Plotting elipses
-            plot_covariance_ellipse(mean, cov=cov, fc='r', alpha=0.20)
+    # Plotting meditions
+    x = []
+    y = []
+    for measure in measurements:
+        if not measure: continue
+        x.append(measure[0])
+        y.append(measure[1])
 
-            # plotting velocity
-            plt.quiver(x_pos,y_pos, x_vel,y_vel, color='b', scale_units='xy', scale=1,alpha=0.50,width=0.005)
+    plt.plot(x, y, '.g', lw=1, ls='--')
 
-        #Plotting meditions
-        x = []
-        y = []
-        for measure in measurements:
-            if not measure: continue
-            x.append(measure[0])
-            y.append(measure[1])
-
-        plt.plot(x, y, '.g', lw=1, ls='--')
-
-
-
-        plt.axis('equal')
-        plt.show()
+    plt.axis('equal')
+    plt.show()
 
 
 if __name__ == '__main__':
@@ -90,23 +83,42 @@ if __name__ == '__main__':
     # lineal example
     # zs = ([0,0],[1,1],[2,2],[3,3],[4,4],[5,5],None,None,None,None,[10,10],None,None,[13,13],[14,14],[15,15]) #Andando en diagonal
     # Curve Example
-    # zs = ([0, 0], [1, 1], [2, 2], [3, 3], [4, 4], [5, 4], [6, 4], [7, 4], None, None, None, [11, 4], [12, 4], [13, 4],
-    #       [15, 4], None, None)
+    # zs = ([0, 0], [1, 1], [2, 2], [3, 3], [4, 4], [5, 4], [6, 4], [7, 4], None, None, None, [13,3],[14,2],[15,1],[16,0], None, None)
+    #
+    # zs = [] #Curva sinusoidal
+    # x = np.arange(0, 10, 0.5)
+    # for xn in x: zs.append([xn,np.sin(xn)])
+    # zs.append(None)
+    # zs.append(None)
+    # zs.append(None)
 
-    zs = [] #Curva sinusoidal
-    x = np.arange(0, 10, 0.5)
-    for xn in x: zs.append([xn,np.sin(xn)])
+    zs = []
+    x = np.arange(0, 10, 1)
+    for xn in x:
+        zs.append([xn, xn])
+
     zs.append(None)
     zs.append(None)
     zs.append(None)
 
-    last_time = 0 #tiempo en el que se tomo la ultima medida
+    x = np.arange(16, 30, 1)
+    for xn in x:
+        zs.append([xn, 15])
+
+    zs.append(None)
+    zs.append(None)
+    zs.append(None)
+    zs.append(None)
+    zs.append(None)
+    zs.append(None)
+
+    last_time = 0  # tiempo en el que se tomo la ultima medida
 
     predictions = []
     covariances = []
     for index, z in enumerate(zs):
 
-        if index == 0: continue #la primera medicion la usamos para inizializar el tracker
+        if index == 0: continue  # la primera medicion la usamos para inizializar el tracker
 
         print (".........Medicion......... ", z)
         dt = 1
@@ -116,15 +128,14 @@ if __name__ == '__main__':
         predictions.append(predX)
         covariances.append(predP)
 
-
         print "Predicha", predX[0], predX[2], "Con velocidad", predX[1], predX[3]
 
         # Si no hay medicion no actualizo el filtro
         if not z:
             continue
 
-        upX,upP = a.update(z)
-        print "Adaptada", upX[0],upX[2], "Con velocidad", upX[1],upX[3]
+        upX, upP = a.update(z)
+        print "Adaptada", upX[0], upX[2], "Con velocidad", upX[1], upX[3]
         print "Siguiente probabilidad", np.diag(upP)
 
         print ("----------------------------")
