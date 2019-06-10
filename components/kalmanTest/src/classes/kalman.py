73c49d76e617babc357da6499223ad99c37687a6
aracelivegamagro <aracelivegamagro@hotmail.com>
2019-06-10 15:11:39 +0200

    Modified KalmanTest to show the predicted result compared with the recieved ones

--- components/kalmanTest/src/classes/kalman.py
+++ components/kalmanTest/src/classes/kalman.py
@@ -1,40 +1,45 @@
 import numpy as np
-from filterpy.common import Q_discrete_white_noise, pprint
+from filterpy.common import Q_discrete_white_noise
 from filterpy.kalman import KalmanFilter
-from filterpy.stats import plot_covariance_ellipse, plot_covariance, plot_gaussian_pdf
+from filterpy.stats import plot_covariance_ellipse, plot_covariance
 from scipy.linalg import block_diag
+# import faulthandler
+# faulthandler.enable()
 import matplotlib.pyplot as plt
-import scipy.linalg as linalg
-import math
 
 
 class KalmanTracker(KalmanFilter):
     def __init__(self, x=0, y=0):
-        super(KalmanTracker, self).__init__(dim_x=6, dim_z=2)
+        super(KalmanTracker, self).__init__(dim_x=4, dim_z=2)
+
+        self.init_to_position(x, y)
+
+    def init_to_position(self, x, y):
         # Measurement Function
-        self.H = np.array([[1, 0, 0, 0, 0, 0],
-                           [0, 0, 0, 1, 0, 0]])
+        self.H = np.array([[1, 0, 0, 0],
+                           [0, 0, 1, 0]])
 
         # Measurement Noise Matrix
-        self.R = np.eye(2) * 0.5  # 0.5 meters2 error
+        self.R = np.eye(2) * 0.5  # 0.1 meters error
+        # Process Noise Matrix
+        q = Q_discrete_white_noise(dim=2, dt=1, var=0.01)
+        self.Q = block_diag(q, q)
 
-        # Initial Position X, vel X, ac X, Y, vel Y, ac Y
-        self.x = np.array([[x, 0, 0, y, 0, 0]]).T
+        # Initial Position X, vel X, Y, vel Y
+        self.x = np.array([[x, 0, y, 0]]).T
 
         # Covariance Matrix
-        self.P = self.P * 2.5
+        self.P = np.eye(4) * 0.5
 
     def predict_with_time_diff(self, dt):
-        self.F = np.array([[1., dt, .5 * dt * dt, 0., 0., 0.],
-                           [0., 1., dt, 0., 0., 0.],
-                           [0., 0., 1., 0., 0., 0.],
-                           [0., 0., 0., 1., dt, .5 * dt * dt],
-                           [0., 0., 0., 0., 1., dt],
-                           [0., 0., 0., 0., 0., 1.]])
+        self.F = np.array([[1, dt, 0, 0],
+                           [0, 1, 0, 0],
+                           [0, 0, 1, dt],
+                           [0, 0, 0, 1]])
 
         # # Process Noise Matrix
-        q = Q_discrete_white_noise(dim=3, dt=dt, var=0.01)
-        self.Q = block_diag(q, q)
+        # q = Q_discrete_white_noise(dim=2, dt=dt, var=0.01)
+        # self.Q = block_diag(q, q)
 
         self.predict()
 
@@ -45,33 +50,31 @@ class KalmanTracker(KalmanFilter):
         return self.x, self.P
 
 
-def plotResults(measurements, predictions, covariances):
-    for pos_vel, cov_matrix in zip(predictions, covariances):
-        # cov = np.array([[cov_matrix[1, 1], cov_matrix[4, 1]], [cov_matrix[1, 4], cov_matrix[4, 4]]])  # cov de vx con vy
-        cov = np.array([[cov_matrix[0, 0], cov_matrix[3, 0]],[cov_matrix[0, 3], cov_matrix[3, 3]]]) #cov de x con y
-
-        [x_pos, x_vel, x_ac, y_pos, y_vel, y_ac] = pos_vel[:, 0]
-        mean = (x_pos, y_pos)
-
-        # Plotting elipses
-        plot_covariance(mean, cov=cov, fc='r', alpha=0.2)
-
-        # plotting velocity
-        plt.quiver(x_pos, y_pos, x_vel, y_vel, color='b', scale_units='xy', scale=1, alpha=0.8, width=0.0035)
-        # plotting acceleration
-        plt.quiver(x_pos, y_pos, x_ac, y_ac, color='y', scale_units='xy', scale=1, alpha=0.8, width=0.0035)
-
-    x = []
-    y = []
-    for measure in measurements:
-        if not measure: continue
-        x.append(measure[0])
-        y.append(measure[1])
-
-    plt.plot(x, y, 'og', lw=1, ls='--')
-
-    # plt.axis('equal')
-    plt.show()
+# def plotResults(measurements, predictions, covariances):
+#     for pos_vel, cov_matrix in zip(predictions, covariances):
+#         cov = np.array([[cov_matrix[0, 0], cov_matrix[2, 0]], [cov_matrix[0, 2], cov_matrix[2, 2]]])
+#         # cov = np.array([[cov_matrix[1, 1], cov_matrix[3, 1]],[cov_matrix[1, 3], cov_matrix[3, 3]]])
+#
+#         [x_pos, x_vel, y_pos, y_vel] = pos_vel[:, 0]
+#         mean = (x_pos, y_pos)
+#         # Plotting elipses
+#         plot_covariance_ellipse(mean, cov=cov, fc='r', alpha=0.20, show_semiaxis=True)
+#
+#         # plotting velocity
+#         plt.quiver(x_pos, y_pos, x_vel, y_vel, color='b', scale_units='xy', scale=1, alpha=0.50, width=0.005)
+#
+#     # Plotting meditions
+#     x = []
+#     y = []
+#     for measure in measurements:
+#         if not measure: continue
+#         x.append(measure[0])
+#         y.append(measure[1])
+#
+#     plt.plot(x, y, '.g', lw=1, ls='--')
+#
+#     plt.axis('equal')
+#     plt.show()
 
 
 if __name__ == '__main__':
@@ -82,17 +85,13 @@ if __name__ == '__main__':
     # zs = ([0,0],[1,1],[2,2],[3,3],[4,4],[5,5],None,None,None,None,[10,10],None,None,[13,13],[14,14],[15,15]) #Andando en diagonal
     # Curve Example
     # zs = ([0, 0], [1, 1], [2, 2], [3, 3], [4, 4], [5, 4], [6, 4], [7, 4], None, None, None, [13,3],[14,2],[15,1],[16,0], None, None)
-
+    #
     zs = [] #Curva sinusoidal
     x = np.arange(0, 20, 0.5)
-    for xn in x:
-        zs.append([xn,np.sin(xn)])
-
+    for xn in x: zs.append([xn,np.sin(xn)])
     zs.append(None)
     zs.append(None)
     zs.append(None)
-
-
 
     # zs = []
     # x = np.arange(0, 10, 1)
@@ -113,7 +112,6 @@ if __name__ == '__main__':
     # zs.append(None)
     # zs.append(None)
     # zs.append(None)
-    #
 
     last_time = 0  # tiempo en el que se tomo la ultima medida
 
@@ -127,31 +125,20 @@ if __name__ == '__main__':
         dt = 1
         last_time = index
         predX, predP = a.predict_with_time_diff(dt)
-        [x_pos, x_vel, x_ac, y_pos, y_vel, y_ac] = predX[:, 0]
-
+        # predX, predP = person_tracker.predict(dt)
         predictions.append(predX)
         covariances.append(predP)
 
-        print "---- Predict ----"
-        print "Posicion", x_pos, y_pos
-        print "Velocidad", x_vel, y_vel
-        print "Aceleracion ", x_ac, y_ac
-        print "Probabilidad", np.diag(predP)
+        print "Predicha", predX[0], predX[2], "Con velocidad", predX[1], predX[3]
 
         # Si no hay medicion no actualizo el filtro
         if not z:
             continue
 
         upX, upP = a.update(z)
-        [x_pos, x_vel, x_ac, y_pos, y_vel, y_ac] = upX[:, 0]
-
-        print "---- Update ----"
-        print "Posicion", x_pos, y_pos
-        print "Velocidad", x_vel, y_vel
-        print "Aceleracion ", x_ac, y_ac
-        print "Probabilidad", np.diag(upP)
+        print "Adaptada", upX[0], upX[2], "Con velocidad", upX[1], upX[3]
+        print "Siguiente probabilidad", np.diag(upP)
 
         print ("----------------------------")
 
     plotResults(zs, predictions, covariances)
-
--- components/kalmanTest/src/specificworker.py
+++ components/kalmanTest/src/specificworker.py
@@ -17,40 +17,129 @@
 #    along with RoboComp.  If not, see <http://www.gnu.org/licenses/>.
 #
 
-from genericworker import *
+import datetime
+from Queue import Queue
+from collections import deque
 
-# If RoboComp was compiled with Python bindings you can use InnerModel in Python
-# sys.path.append('/opt/robocomp/lib')
-# import librobocomp_qmat
-# import librobocomp_osgviewer
-# import librobocomp_innermodel
+import matplotlib.pyplot as plt
+import numpy as np
+from filterpy.stats import plot_covariance
+from genericworker import *
+from classes.kalman_acceleration import KalmanTracker
+
+
+class dataToRepresent():
+    def __init__(self):
+        self.prediction = None
+        self.covariance = None
+        self.measurement = None
+
 
 class SpecificWorker(GenericWorker):
-	def __init__(self, proxy_map):
-		super(SpecificWorker, self).__init__(proxy_map)
-		self.timer.timeout.connect(self.compute)
-		self.Period = 2000
-		self.timer.start(self.Period)
+    def __init__(self, proxy_map):
+        super(SpecificWorker, self).__init__(proxy_map)
+        self.timer.timeout.connect(self.compute)
+        self.Period = 1000
+        self.tracker = None
+        self._last_time_detected = -1
+        self._last_time_predicted = -1
 
-	def __del__(self):
-		print 'SpecificWorker destructor'
+        self.kalman_initialized = False
+        self._detection_queue = Queue()
 
-	def setParams(self, params):
-		return True
+        self._data_queue = deque(maxlen=4)
 
-	@QtCore.Slot()
-	def compute(self):
-		print 'SpecificWorker.compute...'
+        self.timer.start(self.Period)
 
-		return True
+    def __del__(self):
+        print 'SpecificWorker destructor'
+
+    def setParams(self, params):
+        return True
+
+    def plotResults(self):
+        plt.clf()
+        plt.ion()
 
 
-	#
-	# obtainHumanPose
-	#
-	def obtainHumanPose(self, humansFromCam):
-		#
-		#subscribesToCODE
-		#
-		pass
+        for d in self._data_queue:
+            pos_vel = d.prediction
+            cov_matrix = d.covariance
+            measure = d.measurement
 
+            if cov_matrix is None: return
+
+            cov = np.array([[cov_matrix[0, 0], cov_matrix[3, 0]], [cov_matrix[0, 3], cov_matrix[3, 3]]])
+            [x_pos, x_vel, x_ac, y_pos, y_vel, y_ac] = pos_vel[:, 0]
+            # cov = np.array([[cov_matrix[0, 0], cov_matrix[2, 0]], [cov_matrix[0, 2], cov_matrix[2, 2]]])
+            # [x_pos, x_vel, y_pos, y_vel] = pos_vel[:, 0]
+            mean = (x_pos, y_pos)
+
+            plot_covariance(mean, cov=cov, fc='r', alpha=0.2)
+
+            if measure is not None:
+
+                plt.plot(measure[0], measure[1], '.g', lw=1, ls='--')
+
+        plt.quiver(x_pos, y_pos, x_vel, y_vel, color='b', scale_units='xy', scale=1, alpha=0.8, width=0.0035)
+        # 	# # plotting acceleration
+        plt.quiver(x_pos, y_pos, x_ac, y_ac, color='y', scale_units='xy', scale=1, alpha=0.8, width=0.0035)
+
+        # plt.show()
+
+        plt.xlim([-10,10])
+        plt.ylim([-10, 10])
+        plt.pause(0.0001)
+
+        plt.show()
+
+    @QtCore.Slot()
+    def compute(self):
+
+        data = dataToRepresent()
+        if self.kalman_initialized:
+
+            current_time = datetime.datetime.now()
+            dt = (current_time - self._last_time_predicted).total_seconds()
+            predicted, cov = self.tracker.predict_with_time_diff(dt)
+            self._last_time_predicted = current_time
+
+            print "Dato predicho", predicted[0], predicted[2]
+            data.prediction = predicted
+            data.covariance = cov
+
+        if (self._detection_queue.empty() == False):
+            humansFromCam = self._detection_queue.get_nowait()
+            position = humansFromCam.humanList[0].pos
+            detection_time = datetime.datetime.now()
+            self._last_time_detected = detection_time
+            self._last_time_predicted = detection_time
+
+            if (self.kalman_initialized == False):
+                print "Iniciamos kalman en ", position.x / 1000, position.z / 1000
+                self.tracker = KalmanTracker(position.x / 1000, position.z / 1000)
+                self.kalman_initialized = True
+
+            else:
+                print "Dato recibido", position.x / 1000, position.z / 1000
+                data.measurement = [position.x / 1000, position.z / 1000]
+                predicted, cov = self.tracker.update([position.x / 1000, position.z / 1000])
+
+        else:
+            data.measurement = None
+
+        if (data.covariance is not None):
+            self._data_queue.append(data)
+
+
+
+            self.plotResults()
+
+
+            # raw_input('Press Enter')
+
+    #
+    # obtainHumanPose
+    #
+    def obtainHumanPose(self, humansFromCam):
+        self._detection_queue.put(humansFromCam)
