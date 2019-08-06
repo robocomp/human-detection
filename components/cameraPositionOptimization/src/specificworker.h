e81c72a5d7c27f3cea16f45182115a663b7e3639
rcintas <rcintas@unex.es>
2019-08-06 20:14:29 +0200

    randomized camera values change

--- components/cameraPositionOptimization/src/specificworker.h
+++ components/cameraPositionOptimization/src/specificworker.h
@@ -28,6 +28,8 @@
 #define SPECIFICWORKER_H
 
 #include <fstream>
+#include <iostream>
+#include <random>
 
 #include <genericworker.h>
 #include <innermodel/innermodel.h>
@@ -35,14 +37,20 @@
 class SpecificWorker : public GenericWorker
 {
 Q_OBJECT
+private:
+	int cameraChanged;
+	QVec savedCamera;
+	std::vector<QVec> cameras;
 public:
 	SpecificWorker(TuplePrx tprx);
 	~SpecificWorker();
 	bool setParams(RoboCompCommonBehavior::ParameterList params);
 	QVec converToWorld(QString camera, float tx, float ty, float tz, float rx, float ry, float rz);
-	void setCameraPositions();
+	void updateCameraPosition(string camera, QVec values);
+	void randomCameraChange();
 
-	RTMat cam1, cam2, cam3;
+	int randomValue(int min, int max);
+	void restoreCameraValues();
 	float euclidean3D_distance(const QVec &p1, const QVec &p2);
 
 
