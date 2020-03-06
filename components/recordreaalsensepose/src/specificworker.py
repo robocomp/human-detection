4ac2753d1fb88faea9c3a6866821418c7aada64a
rcintas <rcitnas@unex.es>
2020-03-06 13:57:58 +0100

    Recording realsense camera pose on file

--- components/recordreaalsensepose/src/specificworker.py
+++ components/recordreaalsensepose/src/specificworker.py
@@ -21,22 +21,40 @@
 
 from genericworker import *
 
-# If RoboComp was compiled with Python bindings you can use InnerModel in Python
-# sys.path.append('/opt/robocomp/lib')
-# import librobocomp_qmat
-# import librobocomp_osgviewer
-# import librobocomp_innermodel
+import pyrealsense2 as rs
+import json
 
 class SpecificWorker(GenericWorker):
 	def __init__(self, proxy_map):
 		super(SpecificWorker, self).__init__(proxy_map)
 		self.timer.timeout.connect(self.compute)
-		self.Period = 2000
+		self.Period = 30
+		
+
+	def initialize(self):
+		# realsense configuration
+		try:
+			self.pipeline = rs.pipeline()
+			config = rs.config()
+			config.enable_stream(rs.stream.pose)
+			cfg = self.pipeline.start(config)
+		except Exception as e:
+			print("Error initializing camera")
+			print(e)
+			sysexit(-1)
+		#open write file
+		self.outfile = open('realsense_pose.txt', 'w')
+		self.outfile.write('"data_set":[')
 		self.timer.start(self.Period)
 
 
 	def __del__(self):
 		print('SpecificWorker destructor')
+		if self.pipeline:
+			self.pipeline.stop()
+		if self.outfile:
+			self.outfile.write("]}")
+			self.outfile.close()
 
 	def setParams(self, params):
 		#try:
@@ -44,24 +62,25 @@ class SpecificWorker(GenericWorker):
 		#except:
 		#	traceback.print_exc()
 		#	print("Error reading config params")
+		self.initialize()
 		return True
 
 	@QtCore.Slot()
 	def compute(self):
 		print('SpecificWorker.compute...')
-		#computeCODE
-		#try:
-		#	self.differentialrobot_proxy.setSpeedBase(100, 0)
-		#except Ice.Exception as e:
-		#	traceback.print_exc()
-		#	print(e)
-
-		# The API of python-innermodel is not exactly the same as the C++ version
-		# self.innermodel.updateTransformValues('head_rot_tilt_pose', 0, 0, 0, 1.3, 0, 0)
-		# z = librobocomp_qmat.QVec(3,0)
-		# r = self.innermodel.transform('rgbd', z, 'laser')
-		# r.printvector('d')
-		# print(r[0], r[1], r[2])
-
+		
+		frames = self.pipeline.wait_for_frames()
+		
+		# Fetch pose frame
+		pose = frames.get_pose_frame()
+		if pose:
+			# Print some of the pose data to the terminal
+			data = pose.get_pose_data()
+			print("Frame #{}".format(pose.frame_number))
+			print("Position: {}".format(data.translation))
+			print("Velocity: {}".format(data.velocity))
+			print("Acceleration: {}\n".format(data.acceleration))
+			self.outfile.write('{"cameraId":0,"world":['+str(data.translation.x*1000)+','+str(data.translation.y*1000)+','+str(data.translation.z*1000)+","+str(data.rotation.y)+"]}")
+			self.outfile.write(",\n")
 		return True
 
