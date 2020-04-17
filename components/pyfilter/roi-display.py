a755fa5c4d796287b88d8726e39029e6c20e052c
pbustos <pbustos@unex.es>
2020-04-17 11:21:54 +0200

    displays all detections in each image

--- components/pyfilter/roi-display.py
+++ components/pyfilter/roi-display.py
@@ -1,7 +1,5 @@
-import json, sys, pickle
-import random, time
+import json, sys, pickle, random, time
 import numpy as np
-import pprint
 import itertools
 import cv2
 
@@ -17,22 +15,6 @@ def dataIter(init=0, size=40, end=-1):
         end = len(data)
     while init+size < end:
         sample = data[init:init+size]
-        # obs = []
-        # for s in sample:
-        #     for person in s['people']:
-        #         w = person['roi']['width']
-        #         h = person['roi']['height']
-        #         d = 3
-        #         hist = dict()
-        #         if h*w*d == len(person['roi']['image']):
-        #             roi = np.asarray(person['roi']['image'], np.uint8).reshape(h,w,3)
-        #             hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
-        #             hh = cv2.calcHist([hsv], [0], None, [10], [0, 256])[:,0]
-        #             hs = cv2.calcHist([hsv], [1], None, [10], [0, 256])[:,0]
-        #             hv = cv2.calcHist([hsv], [2], None, [10], [0, 256])[:,0]
-        #             hist = np.concatenate([hh, hs, hv])
-        #             obs.append({'timestamp':s['timestamp'], 'world':person['world'], 'roi':hist}) 
-
         print("Sample elapsed time (ms): ", sample[-1]['timestamp']-sample[0]['timestamp'], " Initial: ", size)
         init = init + size 
         yield sample
@@ -47,12 +29,23 @@ while True:
     sample = next(data)[0]
     camera = sample['cameraId']
     person = sample['people'][0]
-    w = person['roi']['width']
-    h = person['roi']['height']
-    d = 3
-    if h*w*d == len(person['roi']['image']):
-        roi = np.asarray(person['roi']['image'], np.uint8).reshape(w, h, 3)
-        cv2.imshow('Cam_'+str(camera), roi)
+    people = sample['people']
+    rois = []
+    for i,person in enumerate(people):
+        roi = person['roi']
+        w = roi['width']
+        h = roi['height']
+        d = 3
+        if h*w*d == len(roi['image']):
+            rois.append(np.asarray(roi['image'], np.uint8).reshape(w, h, 3))
+    if len(rois) > 0:
+        final = np.zeros((max([r.shape[0] for r in rois]), sum([r.shape[1] for r in rois]), 3), np.uint8)
+        wt=0
+        for img in rois:
+            h, w = img.shape[:2]
+            final[:h, wt:wt+w] = img
+            wt = w
+        cv2.imshow('Cam_'+str(camera), final)
     cv2.waitKey(50)
 
 cv2.destroyAllWindows()
